1dd6e71c5fcf9ca7a038549346d9974508ab46ba
vladyslav-buriak <vladka910@gmail.com>
2025-06-21 19:05:17 +0300

    add Нiчого не знайдено

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -3,19 +3,26 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import CryptoList from "./components/CryptoList";
-import {  CryptoCoin,  } from "./types/coin";
+import { CryptoCoin } from "./types/coin";
+import { HiOutlineSearch } from "react-icons/hi";
 
-export default function HomeClient({coins}:{coins:CryptoCoin[]}) {
-  const [search, setSearch] = useState('');
-  const filterCoins = coins.filter((coin)=>{
-  return  coin.name.toLowerCase().includes(search.toLowerCase())
-  })
- 
+export default function HomeClient({ coins }: { coins: CryptoCoin[] }) {
+  const [search, setSearch] = useState("");
+  const filterCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <main>
       <SearchBar search={search} setSearch={setSearch} />
-            <CryptoList data={filterCoins} />
-      
+      {filterCoins.length > 0 ? (
+        <CryptoList data={filterCoins} />
+      ) : (
+        <div className="flex flex-col items-center justify-center text-zinc-500  dark:text-zinc-400 text-xl mt-10">
+          <HiOutlineSearch />
+          Нічого не знайдено
+        </div>
+      )}
     </main>
   );
 }
