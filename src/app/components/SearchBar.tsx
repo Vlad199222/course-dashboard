bb6d91f5f30ffb20480efb241c88e337873f5757
vladyslav-buriak <vladka910@gmail.com>
2025-06-23 22:40:17 +0300

    redisign searchbar

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 interface SearchBarProps {
   search: string;
@@ -7,12 +7,35 @@ interface SearchBarProps {
 
 export default function SearchBar({ search, setSearch }: SearchBarProps) {
   return (
-    <input
-      type="text"
-      placeholder="Пошук криптовалюти..."
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      className="w-full px-4 py-2 rounded-lg border border-zinc-300 dark:border-zinc-700 bg-white dark:bg-zinc-800 text-zinc-900 dark:text-white focus:ouline-none focus:ring-2 focus:ring-emerald-500 transition-all"
-    />
+    <div className="max-w-md mx-auto mt-10">
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="search"
+          id="default-search"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search Mockups, Logos..."
+          required
+        />
+      </div>
+    </div>
   );
 }
