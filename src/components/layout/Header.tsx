1bb56fb9b4b74678b1042853e1a5356dd6f4b4e5
vladyslav-buriak <vladka910@gmail.com>
2025-07-19 21:33:29 +0300

    feat:базовий Header з кнопками логину/реєстрации + shadcn

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
   return (
-    <header className="bg-gray-900 text-white p-4">
-      <h1 className="text-2xl font-bold ">Кріпто Дашборд</h1>
+    <header className="sticky top-0 z-50 bg-white text-white p-6 flex justify-between shadow-md backdrop-blur-sm">
+      <Link className="text-xl font-bold text-black " href="/">
+        CryptoDashboard
+      </Link>
+      <div className="flex gap-2">
+        <Button variant="secondary">Увійти</Button>
+        <Button>Зареєструватися</Button>
+      </div>
     </header>
   );
 };
