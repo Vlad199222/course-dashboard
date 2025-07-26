0e75444b88d9fe8cee11af3cb0e6b35ff123fb1d
vladyslav-buriak <vladka910@gmail.com>
2025-07-27 02:50:24 +0300

    додано хук перевiрка бану при логiнi

--- src/hooks/useHybridUserCheck.ts
+++ src/hooks/useHybridUserCheck.ts
@@ -2,33 +2,52 @@ import { useEffect } from "react";
 import { useAuthStore } from "./useAuthStore";
 
 export const useHybridUserCheck = () => {
-  const { user, setUser, logout } = useAuthStore();
+  const { user, setUser, logout, setAutoLoginFinished, isAutoLoginFineshed } =
+    useAuthStore();
+
   useEffect(() => {
-    if (!user) return;
-    if (!user.banned) return;
+    // Эмулируем завершение авто-логина
+    if (!isAutoLoginFineshed) {
+      console.log("ждем завершение автологина");
+      return;
+    }
 
     const checkBannedStatus = async () => {
       try {
-        const res = await fetch("/api/auth/me");
-        const data = await res.json();
-        if (!res.ok || data?.user) {
-          console.warn("Не вийшло отримати користувача з бази");
+        const res = await fetch("/api/auth/me", {
+          method: "GET",
+          credentials: "include",
+        });
+
+        if (res.status === 403) {
+          alert("❌ Ваш аккаунт заблокирован (403)");
           logout();
           return;
         }
+
+        const data = await res.json();
         const freshUser = data.user;
 
-        if (!freshUser.banned) {
-          setUser(freshUser);
-        } else {
-          alert("Ваш аккаунт заблаковано.");
+        if (freshUser?.banned) {
+          alert("❌ Ваш аккаунт заблокирован (user.banned === true)");
           logout();
+          return;
         }
+
+        setUser(freshUser);
       } catch (error) {
-        console.error("Помилка при перевірці статуса banned", error);
+        console.error("❌ Ошибка при проверке banned", error);
         logout();
       }
     };
+
     checkBannedStatus();
+  }, [isAutoLoginFineshed]);
+
+  useEffect(() => {
+    if (user && !isAutoLoginFineshed) {
+      console.log("автологин завершился");
+      setAutoLoginFinished(true);
+    }
   }, [user]);
 };
