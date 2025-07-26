4a8790c19d8273148dbfab55635552278c4d8622
vladyslav-buriak <vladka910@gmail.com>
2025-07-27 02:45:38 +0300

    добавленна перевiрка user banned

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -6,8 +6,11 @@ import User from "@/models/User";
 
 export async function GET(req: NextRequest) {
   const token = (await cookies()).get("token")?.value;
-  if (!token)
+  console.log(token);
+  if (!token) {
+    console.log("токен отсутствует");
     return NextResponse.json({ message: "Немає токена" }, { status: 401 });
+  }
 
   const secret = process.env.JWT_SECRET;
   if (!secret) return NextResponse.json({ message: "JWT_SECRET не задан" });
@@ -15,6 +18,7 @@ export async function GET(req: NextRequest) {
     const decoded = jwt.verify(token, secret) as jwt.JwtPayload & {
       userId: string;
     };
+    console.log("decoded", decoded);
     await connectToDB();
     const user = await User.findById(decoded.userId).select("-password");
     if (!user) {
@@ -23,7 +27,12 @@ export async function GET(req: NextRequest) {
         { status: 404 }
       );
     }
-    console.log("decoded", decoded);
+
+    if (user.banned) {
+      return NextResponse.json({ message: "User banned" }, { status: 403 });
+    }
+
+    console.log("Юзер найден", user.email);
     return NextResponse.json({ user });
   } catch (error) {
     return NextResponse.json({ message: "Невалідний токен" }, { status: 401 });
