d75af39712306c4105f095abfe56aadd668f3175
vladyslav-buriak <vladka910@gmail.com>
2025-08-02 20:05:54 +0300

    feat: додано API-роут logout,видалення токена

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export const POST = async () => {
+  const response = NextResponse.json({ message: "Logged out" });
+  response.cookies.set("token", "", {
+    httpOnly: true,
+    path: "/",
+    maxAge: 0,
+  });
+  return response;
+};
